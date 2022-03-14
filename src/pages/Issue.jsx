5053358e83b0e1e9bdba4acb04808b8fb4aff757
Jiho Bok <bok.jiho@gmail.com>
2022-03-14 22:55:40 +0900

    [Feat] #6 페이지네이션 기능 구현

--- src/pages/Issue.jsx
+++ src/pages/Issue.jsx
@@ -11,7 +11,6 @@ export default function Issue() {
   const datas = [];
   const [issueDataArr, SetIssueDataArr] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(1);
-  const [issues, setIssues] = useState(new Array(100).fill('hi'));
   const [numOfPages, setNumOfPages] = useState(0); // 페이지네이션 인덱스 길이(갯수)
 
   /* data 연동 시 받아올 형식 */
@@ -47,20 +46,17 @@ export default function Issue() {
 
   useEffect(() => {
     // 페이지네이션 인덱스 갯수 계산
-    const len = issues.length;
+    const len = issueDataArr.length;
     const pagesLength = Math.ceil(len / DISPLAY_CARD_LENGTH);
 
-    console.log(pagesLength);
     setNumOfPages(pagesLength);
     // 첫 번째 인덱스로 페이지 초기화
     setCurrentIndex(1);
-  }, [issues]);
+  }, [issueDataArr]);
 
-  const onClickHandler = () => {
+  const changePageIndex = (newIndex) => {
     // 클릭된 페이지 활성화
-    // 1. 이슈 목록 렌더링
-    //
-    // 2. 페이지네이션에서 활성화된 페이지 스타일 적용
+    setCurrentIndex(newIndex);
   };
 
   return (
@@ -73,11 +69,20 @@ export default function Issue() {
       </Buttons>
       <P>hinyc/wanted-codestates-project-10-8 ISSUES</P>
       <IssueList>
-        {issueDataArr.map((dataObj) => (
-          <IssueC dataObj={dataObj} />
-        ))}
+        {issueDataArr
+          .slice(
+            DISPLAY_CARD_LENGTH * (currentIndex - 1),
+            DISPLAY_CARD_LENGTH * currentIndex - 1 + 1,
+          )
+          .map((dataObj) => (
+            <IssueC dataObj={dataObj} />
+          ))}
       </IssueList>
-      <Pagination issues={issues} />
+      <Pagination
+        currentIndex={currentIndex}
+        numOfPages={numOfPages}
+        changePageIndex={changePageIndex}
+      />
     </Container>
   );
 }
