0c40d115b646d99f7be10bfdf03c775148fd49fa
가즈아 <wjsdydxo45@naver.com>
2022-03-14 20:35:21 +0900

    [Feat] #13 검색 결과 무한 스크롤 구현 - veiw 작업 및 common 공용 컴포넌트 추가

---
+++ src/pages/Main.jsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import styled from 'styled-components';
+import RepoContain from '../common/RepoContain';
+
+export default function Main() {
+  return (
+    <>
+      <Container>
+        {/* view 작업 용 */}
+        <LeftBox>
+          <Test>
+            <Test2 />
+            {/* 맵 돌릴 구간 */}
+            <Test3>
+              <RepoContain />
+              <RepoContain />
+              <RepoContain />
+              <RepoContain />
+              <RepoContain />
+              <RepoContain />
+              <RepoContain />
+              <RepoContain />
+              <RepoContain />
+              <RepoContain />
+              <RepoContain />
+            </Test3>
+          </Test>
+        </LeftBox>
+        {/* view 작업 용 */}
+        <RightBox>
+          <RepoContain selectRepo />
+        </RightBox>
+      </Container>
+    </>
+  );
+}
+
+const Container = styled.div`
+  width: 100%;
+  max-width: 1920px;
+  height: 100%;
+  display: flex;
+`;
+
+const LeftBox = styled.div`
+  width: 50%;
+  height: auto;
+  max-height: 100vh;
+  overflow: scroll;
+  padding: 3rem 4.7rem;
+`;
+
+const Test = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+const Test2 = styled.div`
+  width: 100%;
+  height: 20%;
+  background: blue;
+`;
+
+const Test3 = styled.div`
+  width: 100%;
+  height: 80%;
+`;
+
+const RightBox = styled.div`
+  width: 50%;
+  height: auto;
+  padding: 0 4.7rem;
+`;
